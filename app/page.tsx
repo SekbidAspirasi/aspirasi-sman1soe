5604e227ef556b2435ba911dcdf84a9922f4ca7d
SekbidAspirasi <tualakanikodemus@gmail.com>
2026-09-21 23:52:38 +0800

    Ubah teks tentang kami

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ const App = () => {
                 <div className="mt-6 mb-4 bg-black/85 backdrop-blur-md border border-white/20 p-6 md:p-8 rounded-3xl max-w-2xl text-white transition-all duration-500 shadow-2xl">
                   <h3 className="text-xl font-black text-[#E31837] mb-2 uppercase tracking-wider">Tentang Platform Aspirasi</h3>
                   <p className="text-sm text-gray-200 leading-relaxed mb-4">
-                    Platform resmi siswa SMA Negeri 1 Soe untuk menyalurkan kritik, saran, dan ide kreatif secara aman, transparan, dan privat langsung kepada pengurus OSIS. Dan dikelola secara profesional oleh Sekbid Aspirasi serta kerja sama antara Ketua Osis dan Wakil Ketua Osis SMAN 1 Soe.
+                    Platform resmi siswa SMA Negeri 1 Soe untuk menyalurkan kritik, saran, dan ide kreatif secara aman, transparan, dan privat langsung kepada pengurus OSIS. Dan dikelola secara profesional oleh Sekbid Aspirasi serta kerja sama antara Ketua Osis dan Wakil Ketua Osis SMA Negeri 1 Soe.
                   </p>
                   <div className="border-t border-white/10 pt-3 text-xs text-gray-400">
                     📍 Jl. Prof. Dr. W. Z. Yohanes No. 33, Soe, Kab. Timor Tengah Selatan, NTT.
